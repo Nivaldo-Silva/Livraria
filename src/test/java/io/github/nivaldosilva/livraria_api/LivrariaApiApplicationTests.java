f1bdb0dc694e2733183f7e17107da7fe280b28f1
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2025-03-15 11:09:42 -0300

    Configuração inicial do Projeto

---
+++ src/test/java/io/github/nivaldosilva/livraria_api/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nivaldosilva.livraria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
